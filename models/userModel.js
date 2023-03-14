6551a17652158ad446800ef6bfed20a53051d065
Maradho <maradho@gmail.com>
2023-03-14 17:50:11 +0700

    added photo into table user

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  Photo: {
+    type: String,
+    trim: true,
+  },
   Position: {
     type: String,
     trim: true,
